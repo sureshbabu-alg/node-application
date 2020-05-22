73ec3b9df9ce6df2cc660df4ce42265d6e6c3066
tsureshbab <telugu_sureshbabu@intuit.com>
2020-05-22 14:26:41 +0530

    added requirements.txt

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
-import os
 from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as req:
+    requirements = [line.strip() for line in req]
 
 setup(
     name='test',
     version='0.0.0',
-
     packages=find_packages(),
+    python_version='3.7',
     include_package_data=True,
-
-    install_requires=[
-        "pyyaml==5.1.2",
-        "boto3==1.9.234",
-        "jsonschema",
-        "simplejson",
-        "SQLAlchemy==1.3.11",
-        "PyMySQL==0.9.3"
-    ]
+    install_requires=requirements,
 )
