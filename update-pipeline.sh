8c3be0dded849e7497cdcdf89f606d972bbe9fc4
suresh <telugu_sureshbabu@intuit.com>
2020-06-08 02:53:57 +0530

    test

--- update-pipeline.sh
+++ update-pipeline.sh
@@ -14,7 +14,7 @@ fi
 set -eu
 
 aws cloudformation update-stack \
-        --capabilities CAPABILITY_IAM,CAPABILITY_NAMED_IAM \
+        --capabilities CAPABILITY_IAM \
         --stack-name $CODEPIPELINE_STACK_NAME \
         --parameters ParameterKey=GitHubOAuthToken,ParameterValue=${1} \
         --template-body file://pipeline.yaml
